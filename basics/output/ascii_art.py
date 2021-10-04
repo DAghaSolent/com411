ce2945863654b64bb1fbb48c119ee02541bee167
Danny Agha <5aghad30@solent.ac.uk>
2021-10-04 14:57:05 +0100

    Created a ascii box and made a cool ascii art robot

--- basics/output/ascii_art.py
+++ basics/output/ascii_art.py
@@ -3,3 +3,19 @@ print("#########")
 print("#       #")
 print("#       #")
 print("#########")
+print("\n")
+
+#Robot Asci art
+
+print("\t\t \_/")
+print("\t\t(* *)")
+print("\t   __)#(__")
+print("\t  ( )...( )(_)")
+print("\t  || |_| ||//")
+print("   >==() | | ()/")
+print("\t   _(___)_")
+print("\t  [-]   [-]")
+print("\n")
+print("\t My name is Bender Please insert girder")
+
+
