1ed8c80ee9bcf5f303a446b8603754cca98a8dcb
Danny Agha <5aghad30@solent.ac.uk>
2021-10-08 11:46:18 +0100

    Added code to demonstrate the use of a modulos operator

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,9 @@
+print("Please enter a whole number.")
+number = int(input())
+
+if number % 2 == 0:
+    print(f"The {number} is a even number")
+else:
+    print(f"The {number} is an odd number")
+
+print("The modulo operator machine is complete")
