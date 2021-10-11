bb0820ac97b018f32d7569c8b84f2bc5c1f9a20b
Danny Agha <5aghad30@solent.ac.uk>
2021-10-11 14:14:07 +0100

    Added code to demonstrate the use of a while loop with len function

---
+++ basics/repititions/while_loop/len.py
@@ -0,0 +1,12 @@
+#Asks user for phrase
+print("Please enter a phrase:")
+greeting = len(input())
+#Declare a control variable
+greeting_count = 0
+
+while greeting_count < greeting:
+    print("Bop ", end="")
+    greeting_count += 1
+
+
+
