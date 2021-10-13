5c5ef3c9f1281f6c77c084ed9057424f6d466876
Danny Agha <5aghad30@solent.ac.uk>
2021-10-13 14:46:07 +0100

    Added code to demnostrate the use of a simple for loop.

---
+++ basics/repititions/for_loop/simple.py
@@ -0,0 +1,19 @@
+print("How many mountains should I display")
+mountains = int(input())
+
+for count in range(mountains):
+    print("Displaying")
+    print()
+    print("""
+           __
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+
+  """)
+
+print()
+print("Done")
+
--- basics/repititions/week2_lectures/nested_for.py
+++
@@ -1,2 +0,0 @@
-print("Please enter number of rows:")
-rows =
