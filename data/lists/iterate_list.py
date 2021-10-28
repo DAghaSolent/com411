d053eaf0df8b7bdbabb49a37cde8a789080d2f34
Danny Agha <5aghad30@solent.ac.uk>
2021-10-28 11:32:39 +0100

    Added code to demonstrate how to iterate a list.

---
+++ data/lists/iterate_list.py
@@ -0,0 +1,16 @@
+def directions():
+    directions = ["Move Forward","Move Backward","Turn Left","Turn Right"]
+    return directions
+
+def menu():
+    print("Please select a direction:")
+    print()
+    path = directions()
+    for index in range(len(path)):
+        print(f"{index}: {path[index]}")
+
+def run():
+    menu()
+
+if __name__ == "__main__":
+    run()
