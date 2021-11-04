d1d58a16b9f8ac9ce486a3221549fbe1d7a6b5cb
Danny Agha <5aghad30@solent.ac.uk>
2021-11-04 12:26:19 +0000

    Added code to demonstrate a simple dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,11 @@
+def pattern():
+    sequences = {"Short Sequence": 3,
+                 "Medium Sequence": 2,
+                 "Long Sequence": 1
+                 }
+    print(sequences)
+def run():
+    pattern()
+
+if __name__ == "__main__":
+    run()
