09bbd5d4865413b5ae40cbf49d73f28cbc97dca7
Danny Agha <5aghad30@solent.ac.uk>
2021-11-07 23:16:45 +0000

    Refactor and fixing of tui.py code.

--- data/olympics/tui.py
+++ data/olympics/tui.py
@@ -1,49 +1,37 @@
 def started(msg=""):
     for dash in range(86):
-        print("-",end="")
+        print("-", end="")
     print(f"\nOperation Started: {msg}...")
-
-started("Reading data from athlete_events.csv")
 
 def completed():
     print("Operation Completed")
     for dash in range(86):
-        print("-",end="")
+        print("-", end="")
 
 def error(msg):
     print(f"Error! {msg}")
 
-error("Invalid Selection!")
-
 def menu():
-    print("Please select one of the following options:\n\n"
-          "[years] List unique years\n"
-          "[tally] Tally up medals\n"
-          "[team] Tally up medals for each team\n"
-          "[exit] Exit the program\n")
-
-    print(f"Your selection: ")
+    print(f"""Please select one of the following options:
+         {"[years]":< 10} List unique years
+         {"[tally]":< 10} Tally up medals
+         {"[team]":<10} Tally up medals for each team
+         {"[exit]":<10} Exit the program
+    """"")
+    menu_selection = input().lower()
+    print(f"Your selection:{menu_selection}")
 
 def display_medal_tally(tally):
     print("Displaying Medal Tally:\n")
-    medal_tally = {"Gold":10,"Silver":5,"Bronze":2}
-    print(medal_tally)
+    print(f"|{'Gold':< 10}|{tally['Gold']:< 10}|")
+    print(f"|{'Silver':< 10}|{tally['Silver']:< 10}|")
+    print(f"|{'Bronze':< 10}|{tally['Bronze']:< 10}|")
 
-display_medal_tally()
-
-def display_team_medal_tally(tally):
+def display_team_medal_tally(team_tally):
     print("Display Team medal tally:\n")
-    team_medal_tally = {
-                        "Denmark/Sweden":{"Gold":6,"Silver":0,"Bronze":2},
-                        "Finland":{"Gold":198,"Silver":263,"Bronze":415},
-                        "Norway":{"Gold":299,"Silver":330,"Bronze":281},
-                        "Satchmo":{"Gold":1, "Silver":0, "Bronze":0},
-                        "Tonga":{"Gold":0, "Silver":1, "Bronze":0},
-                        "Digby":{"Gold":0, "Silver":0, "Bronze":1},
-                        }
-    print(team_medal_tally)
-
-display_team_medal_tally()
+    for team,tally in team_tally.items():
+        print(f"{team}")
+        print(f"\tGold:{tally['Gold']},Silver:{tally['Silver']},Bronze:{tally['Bronze']}")
 
 def display_years(years):
     sorted_years = sorted(years,reverse=True)
