c7922394f6855f9b1d2363ce0f4d17c024e00cf9
Danny Agha <5aghad30@solent.ac.uk>
2021-11-07 23:31:27 +0000

    Refactoring main.py section for olympics case study.

--- data/olympics/main.py
+++ data/olympics/main.py
@@ -3,17 +3,17 @@ import process
 import tui
 
 def read_data(file_path):
+    tui.started(f"Reading data from{file_path}")
+    data = []
     with open(file_path) as csv_file:
-        csv_reader  = csv.reader(csv_file)
-        headings = next(csv_reader)
-        data = []
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
         for line in csv_reader:
             data.append(line)
+        tui.completed()
         return data
 def run():
-    pass
     athlete_data = read_data("athlete_events.csv")
-
     while True:
         selection = tui.menu().lower
         if selection == "years":
