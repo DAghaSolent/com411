41f159f197d638df769a0692284e652b988c597d
Danny Agha <5aghad30@solent.ac.uk>
2021-11-11 15:30:16 +0000

    Added code to display mutiple plots.

--- data/visual/plots/squares.py
+++ data/visual/plots/squares.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
 def small():
-    x_values = [3, 3, 4, 4]
-    y_values = [3, 4, 4, 4]
+    x_values = [3, 3, 4, 4, 3]
+    y_values = [3, 4, 4, 3, 3]
     plt.plot(x_values, y_values, 'r:o')
 def medium():
-    x_values = [2, 2, 5, 5]
-    y_values = [2, 5, 5, 2]
-    plt.plot(x_values, y_values, 'go:')
+    x_values = [2, 2, 5, 5, 2]
+    y_values = [2, 5, 5, 2, 2]
+    plt.plot(x_values, y_values, 'g--s')
 def large():
-    x_values = [1, 1, 6, 6]
-    y_values = [1, 6, 6, 1]
-    plt.plot(x_values, y_values, 'bo:')
+    x_values = [1, 1, 6, 6, 1]
+    y_values = [1, 6, 6, 1, 1]
+    plt.plot(x_values, y_values, 'b-o')
 
 def display(small,medium,large):
     small()
