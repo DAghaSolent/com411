afdc75dd21e6df85f53e364b0fdad3a4c0ef3555
DAghaSolent <5aghad30@solent.ac.uk>
2021-12-09 16:02:28 +0000

    Updated Classes human and robot with instance methods.

--- oop/human.py
+++ oop/human.py
@@ -6,18 +6,41 @@ class Human:
         self.age = 0
         self.energy = Human.MAX_ENERGY
 
+
     def __repr__(self):
         return f"human(name={self.name}, age={self.age})"
+
 
     def __str__(self):
         return f"{self.name} is {self.age} years old."
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        if self.energy < 100:
+            self.energy += amount
+        else:
+            print("You have maxed out on your energy levels")
+
+    def distance(self, distance):
+        if self.energy > 0:
+            self.energy -= distance
+        else:
+            print("Your energy is low. You must run the eat method to replenish your energy.")
+
+
     def display(self):
         print(f"I am {self.name}")
+
 
 if(__name__ == "__main__"):
     danny = Human()
     print(danny)
     print()
-    print(repr(danny))
+    print(danny.distance(40))
+    print(danny.energy)
+    print()
+    print(danny.eat(20))
+    print(danny.energy)
 
--- oop/robot.py
+++ oop/robot.py
@@ -10,13 +10,32 @@ class Robot:
         return f"robot(name={self.name}, age={self.age})"
 
     def __str__(self):
-        return f"Robot {self.name} is {self.age} years old."
+        return f"{self.name} is {self.age} years old."
 
     def display(self):
         print(f"I am {self.name}")
+
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        if self.energy < 100:
+            self.energy += amount
+        else:
+            print("You have maxed out on your energy levels")
+
+    def distance(self, distance):
+        if self.energy > 0:
+            self.energy -= distance
+        else:
+            print("Your energy is low. You must run the eat method to replenish your energy.")
 
 if (__name__ == "__main__"):
     bender = Robot()
     print(bender)
     print()
-    print(repr(bender))
+    print(bender.distance(40))
+    print(bender.energy)
+    print()
+    print(bender.eat(20))
+    print(bender.energy)
